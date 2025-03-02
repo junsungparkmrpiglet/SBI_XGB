d83fc0d7d928e22b9f257b0d7fc17f33d70bf18c
junsungparkmrpiglet <mrpiglet@naver.com>
2025-03-03 05:53:58 +0900

    scr

--- scripts/5. Function test.R
+++ scripts/5. Function test.R
@@ -1,168 +1,97 @@
-# function test
-
+# Load libraries
+library(readxl)
+library(xgboost)
+library(dplyr)
 library(tidyr)
 library(pander)
-library(caTools)
+library(openxlsx)
 
-# Read the Excel file
+# Read the development dataset
 dataset1 <- read_excel("C:/download/1. SBI_development.xlsx")
 model <- xgb.load("C:/download/SBI_xgboost_model_train_test_split.model")
 
-#inputs
-#split dataset
-set.seed(2000)
-split <- sample.split (dataset1$SBI, SplitRatio = 0.6)
-train_set <- subset(dataset1, split == TRUE)
-test_set <- subset(dataset1, split == FALSE)
-test.y <- as.matrix(test_set[,1])
-test.x <- as.matrix(test_set[,2:30])
+# Development dataset
+dataset1_x <- as.matrix(dataset1[, 2:30])  # Feature columns
+dataset1$probability_dev <- predict(model, newdata = dataset1_x)
 
-#performance in training test_set
-proability_test <- predict(model, newdata = test.x)
-test_set$proability_test <- cbind(proability_test)
-res = val.prob(test_set$proability_test, test_set$SBI, m=60, cex=.5)
-val_m1 <- val.prob(test_set$proability_test, test_set$SBI,
-                   pl = FALSE)
-pander(val_m1)
+# Performance evaluation for development dataset
+res_dev = val.prob(dataset1$probability_dev, dataset1$SBI, m=60, cex=.5)
+val_dev <- val.prob(dataset1$probability_dev, dataset1$SBI, pl = FALSE)
+pander(val_dev)
 
-#performance in external validation
+# Load external validation dataset
 dataset2 <- read_excel("C:/download/1. SBI_validation.xlsx")
-test.X <- as.matrix(dataset2[,2:30])
-probability_validation <- predict(model, newdata = test.X)
-dataset2$probability_validation <- cbind(probability_validation)
-res = val.prob(dataset2$probability_validation, dataset2$SBI, m=60, cex=.5)
-val_m2 <- val.prob(dataset2$probability_validation, dataset2$SBI,
-                   pl = FALSE)
+dataset2_x <- as.matrix(dataset2[, 2:30])  # Feature columns
+dataset2$probability_validation <- predict(model, newdata = dataset2_x)
+
+# Performance evaluation for validation dataset
+res_val = val.prob(dataset2$probability_validation, dataset2$SBI, m=60, cex=.5)
+val_m2 <- val.prob(dataset2$probability_validation, dataset2$SBI, pl = FALSE)
 pander(val_m2)
 
-
+# Define best thresholds
 best_threshold_cost_based <- 0.088
-best_threshold_youden <-0.335
+best_threshold_youden <- 0.335
 
+# Function to calculate performance metrics
+calculate_metrics <- function(probabilities, true_labels, threshold, dataset_name) {
+  predictions <- ifelse(probabilities >= threshold, 1, 0)
+  
+  TP <- sum(predictions == 1 & true_labels == 1)
+  FN <- sum(predictions == 0 & true_labels == 1)
+  TN <- sum(predictions == 0 & true_labels == 0)
+  FP <- sum(predictions == 1 & true_labels == 0)
+  
+  sensitivity <- TP / (TP + FN)
+  specificity <- TN / (TN + FP)
+  PPV <- TP / (TP + FP)
+  NPV <- TN / (FN + TN)
+  PLR <- sensitivity / (1 - specificity)
+  NLR <- (1 - sensitivity) / specificity
+  Accuracy <- (TP + TN) / (TP + FN + TN + FP)
+  
+  results <- data.frame(
+    Metric = c("Best Threshold", "Sensitivity", "Specificity", "PPV", "NPV", "Positive LR", "Negative LR", "Accuracy"),
+    Value = c(
+      round(threshold, 3),
+      round(sensitivity, 3),
+      round(specificity, 3),
+      round(PPV, 3),
+      round(NPV, 3),
+      round(PLR, 3),
+      round(NLR, 3),
+      round(Accuracy, 3)
+    ),
+    Set = dataset_name
+  )
+  
+  return(results)
+}
 
+# Calculate performance for development dataset
+results_dev_youden <- calculate_metrics(dataset1$probability_dev, dataset1$SBI, best_threshold_youden, "<Development dataset by Youden>")
+results_dev_cost <- calculate_metrics(dataset1$probability_dev, dataset1$SBI, best_threshold_cost_based, "<Development dataset by Cost>")
 
-#Calculating SE/SP by Youden index in test_set
-predictions5 <- ifelse(test_set$proability_test >= best_threshold_youden, 1, 0)
-TP5 <- sum(predictions5 == 1 & test_set$SBI == 1)
-FN5 <- sum(predictions5 == 0 & test_set$SBI == 1)
-TN5 <- sum(predictions5 == 0 & test_set$SBI == 0)
-FP5 <- sum(predictions5 == 1 & test_set$SBI == 0)
-sensitivity5 <- TP5 / (TP5 + FN5)
-specificity5 <- TN5 / (TN5 + FP5)
-PPV5 <- TP5 / (TP5 + FP5)
-NPV5 <- TN5 / (FN5 + TN5)
-PLR5 = sensitivity5 / (1 - specificity5)
-NLR5 = (1 - sensitivity5) / specificity5
-Accuracy5 <- (TP5 + TN5) / (TP5 + FN5 + TN5 + FP5)
-results1 <- data.frame(
-  Metric = c("Best Threshold", "Sensitivity", "Specificity", "PPV", "NPV", "Positive LR", "Negative LR", "Accuracy" ),
-  Value = c(
-    round(best_threshold_youden, 3), 
-    round(sensitivity5, 3), 
-    round(specificity5, 3), 
-    round(PPV5, 3), 
-    round(NPV5, 3),
-    round(PLR5, 3),
-    round(NLR5, 3),
-    round(Accuracy5, 3))
-)
-print(results1)
+# Calculate performance for validation dataset
+results_val_youden <- calculate_metrics(dataset2$probability_validation, dataset2$SBI, best_threshold_youden, "<Validation dataset by Youden>")
+results_val_cost <- calculate_metrics(dataset2$probability_validation, dataset2$SBI, best_threshold_cost_based, "<Validation dataset by Cost>")
 
-#Calculating SE/SP by minimal cost in test_set
-predictions2 <- ifelse(test_set$proability_test >= best_threshold_cost_based, 1, 0)
-TP2 <- sum(predictions2 == 1 & test_set$SBI == 1)
-FN2 <- sum(predictions2 == 0 & test_set$SBI == 1)
-TN2 <- sum(predictions2 == 0 & test_set$SBI == 0)
-FP2 <- sum(predictions2 == 1 & test_set$SBI == 0)
-sensitivity2 <- TP2 / (TP2 + FN2)
-specificity2 <- TN2 / (TN2 + FP2)
-PPV2 <- TP2 / (TP2 + FP2)
-NPV2 <- TN2 / (FN2 + TN2)
-Accuracy2 <- (TP2 + TN2) /(TP2 + FN2 + TN2 + FP2)
-results2 <- data.frame(
-  Metric = c("Best Threshold", "Sensitivity", "Specificity", "PPV", "NPV", "Accuracy"),
-  Value = c(
-    round(best_threshold_cost_based, 3), 
-    round(sensitivity2, 3), 
-    round(specificity2, 3), 
-    round(PPV2, 3), 
-    round(NPV2, 3),
-    round(Accuracy2, 3))
-)
-print(results2)
+# Combine all results
+all_results <- rbind(results_dev_youden, results_dev_cost, results_val_youden, results_val_cost)
 
-#Validation dataset
-#Calculating SE/SP by maximal Youden index from dataset2
-predictions3 <- ifelse(dataset2$probability_validation >= best_threshold_youden, 1, 0)
-TP3 <- sum(predictions3 == 1 & dataset2$SBI == 1)
-FN3 <- sum(predictions3 == 0 & dataset2$SBI == 1)
-TN3 <- sum(predictions3 == 0 & dataset2$SBI == 0)
-FP3 <- sum(predictions3 == 1 & dataset2$SBI == 0)
-sensitivity3 <- TP3 / (TP3 + FN3)
-specificity3 <- TN3 / (TN3 + FP3)
-PPV3 <- TP3 / (TP3 + FP3)
-NPV3 <- TN3 / (FN3 + TN3)
-PLR3 = sensitivity3 / (1 - specificity3)
-NLR3 = (1 - sensitivity3) / specificity3
-Accuracy3 <- (TP3 + TN3) / (TP3 + FN3 + TN3 + FP3)
-results3 <- data.frame(
-  Metric = c("Best Threshold", "Sensitivity", "Specificity", "PPV", "NPV", "Positive LR", "Negative LR", "Accuracy" ),
-  Value = c(
-    round(best_threshold_youden, 3), 
-    round(sensitivity3, 3), 
-    round(specificity3, 3), 
-    round(PPV3, 3), 
-    round(NPV3, 3),
-    round(PLR3, 3),
-    round(NLR3, 3),
-    round(Accuracy3, 3))
-)
-print(results3)
+# Convert results to wide format using `pivot_wider()` (instead of deprecated `spread()`)
+wide_results <- all_results %>%
+  pivot_wider(names_from = Set, values_from = Value)
 
-#Calculating SE/SP by minimal cost (validation set)
-predictions4 <- ifelse(dataset2$probability_validation >= best_threshold_cost_based, 1, 0)
-TP4 <- sum(predictions4 == 1 & dataset2$SBI == 1)
-FN4 <- sum(predictions4 == 0 & dataset2$SBI == 1)
-TN4 <- sum(predictions4 == 0 & dataset2$SBI == 0)
-FP4 <- sum(predictions4 == 1 & dataset2$SBI == 0)
-sensitivity4 <- TP4 / (TP4 + FN4)
-specificity4 <- TN4 / (TN4 + FP4)
-PPV4 <- TP4 / (TP4 + FP4)
-NPV4 <- TN4 / (FN4 + TN4)
-PPV4 <- TP4 / (TP4 + FP4)
-NPV4 <- TN4 / (FN4 + TN4)
-PLR4 = sensitivity4 / (1 - specificity4)
-NLR4 = (1 - sensitivity4) / specificity4
-Accuracy4 <- (TP4 + TN4) / (TP4 + FN4 + TN4 + FP4)
-results4 <- data.frame(
-  Metric = c("Best Threshold", "Sensitivity", "Specificity", "PPV", "NPV", "Positive LR", "Negative LR", "Accuracy" ),
-  Value = c(
-    round(best_threshold_cost_based, 3), 
-    round(sensitivity4, 3), 
-    round(specificity4, 3), 
-    round(PPV4, 3), 
-    round(NPV4, 3),
-    round(PLR4, 3),
-    round(NLR4, 3),
-    round(Accuracy4, 3))
-)
-print(results4)
-
-results1$Set <- "<Test dataset by youden>"
-results2$Set <- "<Test dataset by cost>"
-results3$Set <- "<Validation dataset by youden>"
-results4$Set <- "<Validation dataset  by cost>"
-all_results <- rbind(results1, results2, results3, results4)
-wide_results <- spread(all_results, Set, Value)
+# Order rows based on metrics
 desired_order <- c("Best Threshold", "Sensitivity", "Specificity", "PPV", "NPV", "Positive LR", "Negative LR", "Accuracy")
-wide_results <- wide_results[match(desired_order, wide_results$Metric),]
+wide_results <- wide_results %>%
+  filter(Metric %in% desired_order) %>%
+  arrange(match(Metric, desired_order))
+
 print(wide_results)
 
-# probability save
-library(openxlsx)
-
-write.xlsx(test_set, file = "C:/download/dataset1_predictions.xlsx", sheetName = "Dataset1_Predictions")
-write.xlsx(dataset2, file = "C:/download/dataset2_predictions.xlsx", sheetName = "Dataset2_Predictions")
-train_set$probability_train <- predict(model, newdata = as.matrix(train_set[,2:30]))
-write.xlsx(train_set, file = "C:/download/train_set_predictions.xlsx", sheetName = "Train_Set_Predictions")
- 
+# Save results and predictions
+write.xlsx(dataset1, file = "C:/download/dataset1_predictions.xlsx", sheetName = "Development Predictions")
+write.xlsx(dataset2, file = "C:/download/dataset2_predictions.xlsx", sheetName = "Validation Predictions")
+write.xlsx(wide_results, file = "C:/download/Performance_Results.xlsx", sheetName = "Performance")
